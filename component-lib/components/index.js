bcd89fcdbc1c772b754c6da8e87e90a8fb8e7dfc
CHansenDesign <hello@chansen.design>
2021-11-01 14:35:41 +0100

    started image component in component-lib

--- component-lib/components/index.js
+++ component-lib/components/index.js
@@ -1,14 +1,11 @@
 export * from './Button';
-export * from './Section';
-
-export * from './List';
-export * from './Item';
-
 export * from './Dropdown';
-
 export * from './Form';
-export * from './Input';
-export * from './Textarea';
-
 export * from './IconOnHoverInfo';
+export * from './Image';
+export * from './Input';
+export * from './Item';
+export * from './List';
 export * from './NextLink';
+export * from './Section';
+export * from './Textarea';
